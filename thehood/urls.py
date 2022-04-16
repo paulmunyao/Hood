0168b0bb6be4cfaf05dafdb78184fe0166bce06d
Paul Munyao <paulmunyao094@gmail.com>
2022-04-16 12:47:20 +0300

    Create a url path for profile

--- thehood/urls.py
+++ thehood/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('display', views.display, name='display'),
+    path('profile', views.profile, name='profile'),
 ]
