47c4e75975dc905985036aa9be45e6fc3eb6de86
Paul Munyao <paulmunyao094@gmail.com>
2022-04-16 09:39:34 +0300

    Create signup forms

--- thehood/forms.py
+++ thehood/forms.py
@@ -2,3 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import TextInput, EmailInput, PasswordInput
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(
+        max_length=254,
+        help_text='Required. Inform a valid email address.',
+        widget=EmailInput(attrs={'class': 'form-control'}),
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
+        widgets = {
+            'username': TextInput(attrs={'class': 'form-control'}),
+            'password1': PasswordInput(attrs={'class': 'form-control'}),
+            'password2': PasswordInput(attrs={'class': 'form-control'}),
+        }
