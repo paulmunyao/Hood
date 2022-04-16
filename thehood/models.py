9645df62daff2ae9e9b3bf1e05212cf4096ea4e1
Paul Munyao <paulmunyao094@gmail.com>
2022-04-16 13:04:55 +0300

    Add location and description of the place

--- thehood/models.py
+++ thehood/models.py
@@ -6,6 +6,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(default='default.jpg', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    description = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
