53b3ba987d6ac6af50a862e903b6fb4319c5f3a2
Paul Munyao <paulmunyao094@gmail.com>
2022-04-17 17:30:47 +0300

    Create logic for  a user to create their profile

--- thehood/views.py
+++ thehood/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm
+from .forms import SignUpForm,Profile,UpdateUserForm, UpdateProfileForm
 from .models import Profile
 
 # Create your views here.
@@ -28,4 +29,12 @@ def display(request):
 
 @login_required(login_url='/registration/login')
 def profile(request):
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        profile_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return redirect('profile')
     return render(request, 'users/profile.html')    
