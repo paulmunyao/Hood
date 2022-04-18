fb923e983901816192bb61b6c51a801696a8bf8e
Paul Munyao <paulmunyao094@gmail.com>
2022-04-18 16:54:30 +0300

    Import neighbourhood and business models into forms file

--- thehood/forms.py
+++ thehood/forms.py
@@ -1,7 +1,7 @@
 from logging import PlaceHolder
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Neighbourhood, Business
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import TextInput, EmailInput, PasswordInput
 
