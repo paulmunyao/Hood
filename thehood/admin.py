9e35f682d39d3bd724ba1e6f84acd8c3525865c5
Paul Munyao <paulmunyao094@gmail.com>
2022-04-18 16:53:18 +0300

    Register  models in admin file

--- thehood/admin.py
+++ thehood/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Neighbourhood, Business
 
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
