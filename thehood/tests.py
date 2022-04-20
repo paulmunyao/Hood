428030e74f4fd62b80ba9785a95188739efd5166
Paul Munyao <paulmunyao094@gmail.com>
2022-04-20 08:13:13 +0300

    Indenting words on test cases

--- thehood/tests.py
+++ thehood/tests.py
@@ -2,38 +2,44 @@ from django.test import TestCase
 from .models import Business, Profile, Neighbourhood, User
 
 # Create your tests here.
+
+
 class ProfileTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='test')
-        self.profile = Profile.objects.create(user=self.user, bio='test bio', location='test location', description='test description')
+        self.profile = Profile.objects.create(
+            user=self.user, bio='test bio', location='test location', description='test description')
 
     def test_profile_content(self):
         self.assertEqual(f'{self.user.username} Profile', str(self.profile))
-        self.assertEqual(self.profile.image,'test image')
+        self.assertEqual(self.profile.image, 'test image')
         self.assertEqual(self.profile.bio, 'test bio')
         self.assertEqual(self.profile.location, 'test location')
         self.assertEqual(self.profile.description, 'test description')
 
+
 class NeighbourhoodTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='test')
-        self.neighbourhood = Neighbourhood.objects.create(name='test name', location='test location', occupants=1, admin=self.user)
+        self.neighbourhood = Neighbourhood.objects.create(
+            name='test name', location='test location', occupants=1, admin=self.user)
 
     def test_neighbourhood_content(self):
         self.assertEqual(f'{self.neighbourhood.name}', str(self.neighbourhood))
         self.assertEqual(self.neighbourhood.location, 'test location')
         self.assertEqual(self.neighbourhood.occupants, 1)
         self.assertEqual(self.neighbourhood.admin, self.user)
-            
+
 
 class BusinessTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(username='test')
-        self.business = Business.objects.create(name='test', id='1', email='test')
+        self.business = Business.objects.create(
+            name='test', id='1', email='test')
 
     def test_business_content(self):
         self.assertEqual(f'{self.business.name}', 'test')
         self.assertEqual(f'{self.business.id}', '1')
         self.assertEqual(f'{self.business.neighbourhood}', 'test')
         self.assertEqual(f'{self.business.email}', 'test')
-
+  
